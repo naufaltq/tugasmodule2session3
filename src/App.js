e3bf68d4bb3c4d5389636ecc6ba6e481104a1a7f
Naufal Taufiqurrahman <naufaltq16@gmail.com>
2022-03-28 16:59:12 +0700

    adding data.js, app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
+        </a> */}
+        {data.map((data) => {
+          return (
+            <div>
+              <p>{data.name}</p>
+              <p>{data.id}</p>
+              <p>{data.popularity}</p>
+            </div>
+          );
+        })}
       </header>
     </div>
   );
